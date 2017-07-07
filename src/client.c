7af0a1d5f305c90f45869decd0b63646c5824ead
van90 <petrosyanvan@gmail.com>
2017-07-07 11:41:21 +0400

    Initial commit

---
+++ src/client.c
@@ -0,0 +1,89 @@
+/*
+ ============================================================================
+ Name        : client.c
+ Author      : Van Petrosyan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define BUFSIZE 1024
+
+
+int main(int argc, char **argv) {
+	int sockfd, portno, n;
+	struct sockaddr_in serveraddr;
+	struct hostent *server;
+	char *hostname;
+	char buf[BUFSIZE];
+
+	/* check command line arguments */
+	if (argc != 3) {
+	   fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
+	   exit(0);
+	}
+	hostname = argv[1];
+	portno = atoi(argv[2]);
+
+	/* socket: create the socket */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0){
+		perror("ERROR opening socket");
+		exit(EXIT_FAILURE);
+	}
+
+	/* gethostbyname: get the server's DNS entry */
+	server = gethostbyname(hostname);
+	if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+		exit(EXIT_FAILURE);
+	}
+
+	/* build the server's Internet address */
+	bzero((char *) &serveraddr, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr,
+	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+	serveraddr.sin_port = htons(portno);
+
+	/* connect: create a connection with the server */
+	if (connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0){
+	  perror("ERROR connecting");
+	  exit(EXIT_FAILURE);
+	}
+
+	/* we have connected to the server */
+	/* get message line from the user */
+	printf("Please enter msg: ");
+	bzero(buf, BUFSIZE);
+	fgets(buf, BUFSIZE, stdin);
+
+	/* send the message line to the server */
+	n = write(sockfd, buf, strlen(buf));
+	if (n < 0){
+	  perror("ERROR writing to socket");
+	  exit(EXIT_FAILURE);
+	}
+
+	/* print the server's reply */
+	bzero(buf, BUFSIZE);
+	n = read(sockfd, buf, BUFSIZE);
+	if (n < 0){
+	  perror("ERROR reading from socket");
+	  exit(EXIT_FAILURE);
+	}
+	printf("Echo from server: %s", buf);
+
+	close(sockfd);
+	return EXIT_SUCCESS;
+}
