f97d18206eefb5947d94646f9fe3a4892676e2dc
Van Petrosyan <petrosyanvan@gmail.com>
2017-07-08 22:57:27 +0400

    finished

--- src/client.c
+++ src/client.c
@@ -4,29 +4,38 @@
  Author      : Van Petrosyan
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : client in C, Ansi-style
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <curses.h>
+#include <time.h>
+#include <pthread.h>
+#include <termios.h>
 
 #define BUFSIZE 1024
 
+void *sender();
+void *listener();
+void sigint_handler();
+
+static int sockfd;
 
 int main(int argc, char **argv) {
-	int sockfd, portno, n;
+	int portno;
 	struct sockaddr_in serveraddr;
 	struct hostent *server;
-	char *hostname;
-	char buf[BUFSIZE];
+	char *hostname;	
+	//char buf[BUFSIZE];
 
 	/* check command line arguments */
 	if (argc != 3) {
@@ -63,58 +72,71 @@ int main(int argc, char **argv) {
 	  exit(EXIT_FAILURE);
 	}
 
-	initscr();
-	//noecho();
-	nodelay(stdscr, TRUE);
-	cbreak();
-	keypad(stdscr, TRUE);
-	int ch;
-	int i = 0;
-	char rcv_buf[BUFSIZE];
-	/* we have connected to the server */
-	/* get message line from the user */
-	while(1){
+	// Set up threads
+    pthread_t threads[2];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-		if ((ch = getch()) == ERR){
+    // Spawn the listen/receive deamons    
+    pthread_create(&threads[0], &attr, listener, NULL);
+    pthread_create(&threads[1], &attr, sender, NULL);
 
-		}
-		else{
-			rcv_buf[i++] = ch;
-			if(ch == '\n'){
-				rcv_buf[i] = '\0';
-				n = write(sockfd, rcv_buf, strlen(buf));
-				if (n < 0){
-				  perror("ERROR writing to socket");
-				  exit(EXIT_FAILURE);
-				}
-				i = 0;
-				bzero(rcv_buf, BUFSIZE);
-			}
-		}
+    // init ncurses
+    initscr();
+    nodelay(stdscr, FALSE);				// getch will return ERR if there is no input 
+    //keypad(stdscr, TRUE);
+    noecho();							// don't echo chars
+    raw();								// don't wait for new line
 
-		//bzero(buf, BUFSIZE);
-		//fgets(buf, BUFSIZE, stdin);
+    signal(SIGINT, sigint_handler);		// handle CTRL+C
 
-		/* send the message line to the server */
-		/*n = write(sockfd, buf, strlen(buf));
-		if (n < 0){
-		  perror("ERROR writing to socket");
-		  exit(EXIT_FAILURE);
-		} */
-
-		/* print the server's reply */
-		bzero(buf, BUFSIZE);
-		n = read(sockfd, buf, BUFSIZE);
-		if (n < 0){
-		  perror("ERROR reading from socket");
-		  exit(EXIT_FAILURE);
-		}
-		printf("%s\n", buf);
-	}
-	endwin();
-	close(sockfd);
+	while(1)
+		sleep(1);										
 	return EXIT_SUCCESS;
 }
 
+// Send message from keyboard to server
+void *sender(){
+	int n = 0;
+	char ch;	
+    while(1){	
+		ch = getch();
+		if (ch != ERR){
+			n = write(sockfd, &ch, 1);
+			if (n < 0){
+			  	perror("ERROR writing to socket");
+			  	endwin();
+				pthread_exit(NULL);
+				close(sockfd);
+		  		exit(EXIT_FAILURE);
+			}
+		}
+    }
+}
+
+// Listen for messages and display them
+void *listener(){
+    char rcv_buf[BUFSIZE];
+    int n = 0;
+    while(1) {
+        memset(rcv_buf, 0, BUFSIZE);
+        //Receive message from server
+        n = read(sockfd, rcv_buf, BUFSIZE);
+		if (n < 0){
+		 	perror("ERROR reading from socket");
+		  	endwin();
+			pthread_exit(NULL);
+			close(sockfd);
+		  	exit(EXIT_FAILURE);
+		}   
+		write( STDOUT_FILENO, rcv_buf, strlen( rcv_buf ));
+    }
+}
 
 
+// Catch SIGINT
+void sigint_handler(){
+	char sig = 0x03; // send ctrl+c
+	write(sockfd, &sig, 1);
+}
